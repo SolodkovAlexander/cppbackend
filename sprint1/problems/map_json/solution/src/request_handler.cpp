252d15c32b04d3672ddb16e646814d3582dd3e4e
Kortunov Dmitry <kortunovdmitry@gmail.com>
2024-10-06 01:22:38 +0300

    SPRINT-1 Refactoring in map_json RequestHandler

--- sprint1/problems/map_json/solution/src/request_handler.cpp
+++ sprint1/problems/map_json/solution/src/request_handler.cpp
@@ -2,4 +2,73 @@
 
 namespace http_handler {
 
+json::array RequestHandler::MapsToShortJson(const Game::Maps& maps) {
+    json::array maps_data;
+    for (const auto& map : maps) {
+        maps_data.emplace_back(json::object({
+            {"id", *map.GetId()},
+            {"name", map.GetName()}
+        }));
+    }
+    return maps_data;
+}
+
+json::object RequestHandler::MapToJson(const Map* map) {
+    json::object map_data;
+    map_data["id"] = *(map->GetId());
+    map_data["name"] = map->GetName();
+
+    json::array roads;
+    for (const auto& road : map->GetRoads()) {
+        roads.emplace_back(RequestHandler::RoadToJson(road));
+    }
+    map_data["roads"] = std::move(roads);
+
+    json::array buildings;
+    for (const auto& building : map->GetBuildings()) {
+        buildings.emplace_back(RequestHandler::BuildingToJson(building));
+    }
+    map_data["buildings"] = std::move(buildings);
+
+    json::array offices;
+    for (const auto& office : map->GetOffices()) {
+        offices.emplace_back(RequestHandler::OfficeToJson(office));
+    }
+    map_data["offices"] = std::move(offices);
+
+    return map_data;
+}
+
+json::object RequestHandler::RoadToJson(const Road& road) {
+    json::object road_data({
+        {"x0", road.GetStart().x},
+        {"y0", road.GetStart().y}
+    });
+    if (road.IsHorizontal()) {
+        road_data["x1"] = road.GetEnd().x;
+    } else {
+        road_data["y1"] = road.GetEnd().y;
+    }
+    return road_data;
+}
+
+json::object RequestHandler::BuildingToJson(const Building& building) {
+    return json::object({
+        {"x", building.GetBounds().position.x},
+        {"y", building.GetBounds().position.y},
+        {"w", building.GetBounds().size.width},
+        {"h", building.GetBounds().size.height}
+    });
+}
+
+json::object RequestHandler::OfficeToJson(const Office& office) {
+    return json::object({
+        {"id", *(office.GetId())},
+        {"x", office.GetPosition().x},
+        {"y", office.GetPosition().y},
+        {"offsetX", office.GetOffset().dx},
+        {"offsetY", office.GetOffset().dy}
+    });
+}
+
 }  // namespace http_handler
--- sprint1/problems/map_json/solution/src/request_handler.h
+++ sprint1/problems/map_json/solution/src/request_handler.h
@@ -10,6 +10,7 @@ namespace beast = boost::beast;
 namespace http = beast::http;
 namespace json = boost::json;
 using namespace std::literals;
+using namespace model;
 
 class RequestHandler {
 public:
@@ -19,139 +20,86 @@ public:
 
     RequestHandler(const RequestHandler&) = delete;
     RequestHandler& operator=(const RequestHandler&) = delete;
-    
-    // Ответ, тело которого представлено в виде строки
-    using StringResponse = http::response<http::string_body>;
-    
+
+    template <typename Body, typename Allocator, typename Send>
+    void operator()(http::request<Body, http::basic_fields<Allocator>>&& req, Send&& send) {
+        // Bad request-s bodies
+        static const auto bad_request_response_body = json::serialize(json::object({
+                                                           {"code", "badRequest"s},
+                                                           {"message", "Bad request"s}
+                                                       }));
+        static const auto map_not_found_response_body = json::serialize(json::object({
+                                                            {"code", "mapNotFound"s},
+                                                            {"message", "Map not found"s}
+                                                        }));
+        // Not GET request
+        if (req.method() != http::verb::get) {
+            send(RequestHandler::MakeStringResponse(http::status::bad_request,
+                                                    bad_request_response_body,
+                                                    req));
+            return;
+        }
+
+        // Request: /api/v1/maps
+        if (req.target() == "/api/v1/maps"s) {
+            send(RequestHandler::MakeStringResponse(http::status::ok, 
+                                                    json::serialize(RequestHandler::MapsToShortJson(game_.GetMaps())), 
+                                                    req));
+            return;
+        }
+
+        // Request: /api/v1/maps/{map id}
+        static const auto map_id_regex = std::regex(R"/(/api/v1/maps/(.+))/");
+        std::smatch match_results;
+        std::string target_name(req.target().begin(), req.target().end());
+        if (!regex_match(target_name, match_results, map_id_regex)) {
+            send(RequestHandler::MakeStringResponse(http::status::bad_request,
+                                                    bad_request_response_body,
+                                                    req));
+            return;
+        }
+
+        // Request: /api/v1/maps/{map id}, no map with id
+        auto map = game_.FindMap(model::Map::Id{match_results[1]});
+        if (!map) {
+            send(RequestHandler::MakeStringResponse(http::status::bad_request,
+                                                    map_not_found_response_body,
+                                                    req));
+            return;
+        }
+
+        // Request: /api/v1/maps/{map id}, response
+        send(RequestHandler::MakeStringResponse(http::status::ok, 
+                                                json::serialize(RequestHandler::MapToJson(map)), 
+                                                req));
+        return;
+    }
+
+private:
     struct ContentType {
         ContentType() = delete;
         constexpr static std::string_view APPLICATION_JSON = "application/json"sv;
     };
+    
+    // Ответ, тело которого представлено в виде строки
+    using StringResponse = http::response<http::string_body>;
 
-    // Создаёт StringResponse с заданными параметрами
-    StringResponse MakeStringResponse(http::status status, std::string_view body, unsigned http_version,
-                                    bool keep_alive,
-                                    std::string_view content_type = ContentType::APPLICATION_JSON) {
-        StringResponse response(status, http_version);
-        response.set(http::field::content_type, content_type);
+    template <typename Body, typename Allocator>
+    static StringResponse MakeStringResponse(http::status status, std::string_view body, http::request<Body, http::basic_fields<Allocator>>& request) {
+        StringResponse response(status, request.version());
+        response.set(http::field::content_type, ContentType::APPLICATION_JSON);
         response.body() = body;
         response.content_length(body.size());
-        response.keep_alive(keep_alive);
+        response.keep_alive(request.keep_alive());
         return response;
     }
 
-    template <typename Body, typename Allocator, typename Send>
-    void operator()(http::request<Body, http::basic_fields<Allocator>>&& req, Send&& send) {
-        static const auto make_good_request_body = [&req](const std::string& response) {
-            std::string request_body;
-
-            return request_body;
-        };
-
-        json::object bad_request_body;
-        bad_request_body["code"] = "badRequest"s;
-        bad_request_body["message"] = "Bad request"s;
-
-        if (req.method() != http::verb::get) {
-            send(MakeStringResponse(http::status::bad_request,
-                                    json::serialize(bad_request_body),
-                                    req.version(),
-                                    req.keep_alive()));
-            return;
-        }
-
-        // /api/v1/maps
-        if (req.target() == "/api/v1/maps"s) {
-            // JSON с картами
-            json::array maps;
-            for (const auto& map : game_.GetMaps()) {
-                json::object map_data;
-                map_data["id"] = *map.GetId();
-                map_data["name"] = map.GetName();
-                maps.emplace_back(map_data);
-            }
-            
-            send(MakeStringResponse(http::status::ok,
-                                    json::serialize(maps),
-                                    req.version(),
-                                    req.keep_alive()));
-            return;
-        }
-
-        // /api/v1/maps/{id-карты}
-        static const auto map_id_regex = std::regex(R"/(/api/v1/maps/(.+)?)/");
-        std::smatch match_results;
-        std::string target_name(req.target().begin(), req.target().end());
-        if (!regex_match(target_name, match_results, map_id_regex)) {
-            send(MakeStringResponse(http::status::bad_request,
-                                    json::serialize(bad_request_body),
-                                    req.version(),
-                                    req.keep_alive()));
-            return;
-        }
-        auto map = game_.FindMap(model::Map::Id{match_results[1]});
-        if (!map) {
-            bad_request_body["code"] = "mapNotFound"s;
-            bad_request_body["message"] = "Map not found"s;
-            send(MakeStringResponse(http::status::bad_request,
-                                    json::serialize(bad_request_body),
-                                    req.version(),
-                                    req.keep_alive()));
-            return;
-        }
-
-        // /api/v1/maps/{id-карты}
-        json::object map_data;
-        map_data["id"] = *(map->GetId());
-        map_data["name"] = map->GetName();
-
-        {
-            json::array roads;
-            for (const auto& road : map->GetRoads()) {
-                json::object road_data;
-                road_data["x0"] = road.GetStart().x;
-                road_data["y0"] = road.GetStart().y;
-                if (road.IsHorizontal()) {
-                    road_data["x1"] = road.GetEnd().x;
-                } else {
-                    road_data["y1"] = road.GetEnd().y;
-                }
-                roads.emplace_back(std::move(road_data));
-            }
-            map_data["roads"] = std::move(roads);
-        }
-        {
-            json::array buildings;
-            for (const auto& building : map->GetBuildings()) {
-                json::object building_data;
-                building_data["x"] = building.GetBounds().position.x;
-                building_data["y"] = building.GetBounds().position.y;
-                building_data["w"] = building.GetBounds().size.width;
-                building_data["h"] = building.GetBounds().size.height;
-                buildings.emplace_back(std::move(building_data));
-            }
-            map_data["buildings"] = std::move(buildings);
-        }
-        {
-            json::array offices;
-            for (const auto& office : map->GetOffices()) {
-                json::object office_data;
-                office_data["id"] = *(office.GetId());
-                office_data["x"] = office.GetPosition().x;
-                office_data["y"] = office.GetPosition().y;
-                office_data["offsetX"] = office.GetOffset().dx;
-                office_data["offsetY"] = office.GetOffset().dy;
-                offices.emplace_back(std::move(office_data));
-            }
-            map_data["offices"] = std::move(offices);
-        }
-
-        send(MakeStringResponse(http::status::ok,
-                                json::serialize(map_data),
-                                req.version(),
-                                req.keep_alive()));
-        return;
-    }
+private:
+    static json::array MapsToShortJson(const Game::Maps& maps);
+    static json::object MapToJson(const Map* map);
+    static json::object RoadToJson(const Road& road);
+    static json::object BuildingToJson(const Building& building);
+    static json::object OfficeToJson(const Office& office);
 
 private:
     model::Game& game_;
